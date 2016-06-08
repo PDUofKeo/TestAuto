3c59bc7ba8480f749d26403a424f6d4a48e35778
pdu <pdu@keosys.com>
2016-06-08 11:08:01 +0200

    first commit

---
+++ TestAuto/src/main/java/imagys/com/TestAuto/App.java
@@ -0,0 +1,13 @@
+package imagys.com.TestAuto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
