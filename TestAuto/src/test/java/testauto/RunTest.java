bc13af9a57ca2272fff2ae4478a71de214d0c729
pdu <pdu@KEO158.keosys.local>
2016-06-26 18:27:33 +0200

    one test

--- TestAuto/src/test/java/testauto/RunTest.java
+++ TestAuto/src/test/java/testauto/RunTest.java
@@ -8,7 +8,8 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = { "classpath:features" }, monochrome = true, format = { "pretty", "html:target/cucumber",
 		"rerun:target/rerun.txt", "json:target/report/cucumber.json" }, 
-tags = {"@MAT1,@MAT3,@MAT5,@MAT6,@MAT7,@MAT8,@After" }
+tags = {"@MAT1"}
+//		+ ",@MAT3,@MAT5,@MAT6,@MAT7,@MAT8,@After" }
 // +
 // ",@SupervisionSearch,@SupervisionResult,@checkQCisDeleted,@UserLogout,@SupervisorLogin1"}
 )
